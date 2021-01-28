935567d47ff5ac5198b8c14a9a237e912df084a5
HackerBoiiii <rakeshrawat456@gmail.com>
2021-01-28 13:12:34 +0530

    Initial commit

---
+++ lib/helper/data.dart
@@ -0,0 +1,14 @@
+import 'package:news_api/models/category_model.dart';
+
+List<CategoryModel> getCategories(){
+
+  List<CategoryModel> category=new List<CategoryModel>();
+  CategoryModel categoryModel= new CategoryModel();
+
+  //1
+  categoryModel.categoryName="Bussiness";
+  categoryModel.imageUrl="https://images.unsplash.com/photo-1425421669292-0c3da3b8f529?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1049&q=80";
+
+
+
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Flutter'),
+            Text('News',style: TextStyle(
+              color: Colors.blue
+            ),)
+          ],
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: ,
+      ),
+
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl,categoryname;
+
+  CategoryTile({this.imageUrl,this.categoryname});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Image.network(imageUrl,width: 120,height: 160,),
+        ],
+      ),
+    );
+  }
+}
