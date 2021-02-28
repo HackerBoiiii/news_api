a983f52bff3e594c4b5e9bd004fb62d9e3e3a2a9
rakeshrawat <rakeshrawat456@gmail.com>
2021-02-28 13:20:14 +0530

    project complete with about us secotion

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,12 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_api/helper/data.dart';
 import 'package:news_api/helper/news.dart';
 import 'package:news_api/models/article_model.dart';
 import 'package:news_api/models/category_model.dart';
 import 'package:news_api/views/articleview.dart';
 import 'package:news_api/views/category_news.dart';
+
+import 'aboutus.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -15,8 +18,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<CategoryModel> categories = new List<CategoryModel>();
-  List<ArticleModel> articles=new List<ArticleModel>();
-  bool _loading=true;
+  List<ArticleModel> articles = new List<ArticleModel>();
+  bool _loading = true;
 
   @override
   void initState() {
@@ -26,86 +29,68 @@ class _HomeState extends State<Home> {
     getNews();
   }
 
-  getNews()async{
-    News newsClass =News();
+  getNews() async {
+    News newsClass = News();
     await newsClass.getNews();
-    articles=newsClass.news;
+    articles = newsClass.news;
     setState(() {
-      _loading=false;
+      _loading = false;
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
         child: ListView(
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('FlutterNews'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            UserAccountsDrawerHeader(
+
+              accountName: Text("Rakesh Singh Rawat"),
+              accountEmail: Text("rakeshrawat456@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: new AssetImage('images/sgrrlogo.jpg'),
               ),
             ),
             ListTile(
-              title: Text('Bussiness'),
-              trailing: Icon(Icons.add_business_sharp),
+
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: (
+                  ) {
+                setState(() {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                  );
+                });
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text("Contact Us"),
               onTap: () {
-                // Update the state of the app.
-                // ...
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutUs()),
+                );
               },
             ),
             ListTile(
-              title: Text('Entertainment'),
-              trailing: Icon(Icons.movie),
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Exit"),
               onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('General'),
-              trailing: Icon(Icons.account_balance),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('Health'),
-              trailing: Icon(Icons.healing),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('Sports'),
-              trailing: Icon(Icons.sports),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('Technology'),
-              trailing: Icon(Icons.electric_car_outlined),
-              onTap: () {
-                // Update the state of the app.
-                // ...
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+
               },
             ),
           ],
         ),
       ),
       appBar: AppBar(
-
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -119,45 +104,49 @@ class _HomeState extends State<Home> {
         elevation: 0.0,
         centerTitle: true,
       ),
-      body: _loading ? Center(
-        child: Container(
-
-          child: CircularProgressIndicator(),
-        ),
-      ):Container(
-          margin: EdgeInsets.only(left: 5,right: 5),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(bottom: 10),
-                height: 80,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(bottom: 15),
-                  itemCount: categories.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return CategoryTile(
-                      imageUrl: categories[index].imageUrl,
-                      categoryname: categories[index].categoryName,
-                    );
-                  },
-                ),
+      body: _loading
+          ? Center(
+              child: Container(
+                child: CircularProgressIndicator(),
               ),
-              Flexible(
-                child: Container(
-                  child: ListView.builder(
-
-                    itemCount: articles.length,
-                    shrinkWrap: true,
-                    itemBuilder:(context, index) {
-                      return BlogTile(articles[index].urlToImage, articles[index].title, articles[index].description,articles[index].url);
-                    },
+            )
+          : Container(
+              margin: EdgeInsets.only(left: 5, right: 5),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    height: 80,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      padding: EdgeInsets.only(bottom: 15),
+                      itemCount: categories.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return CategoryTile(
+                          imageUrl: categories[index].imageUrl,
+                          categoryname: categories[index].categoryName,
+                        );
+                      },
+                    ),
                   ),
-                ),
-              )
-            ],
-          )),
+                  Flexible(
+                    child: Container(
+                      child: ListView.builder(
+                        itemCount: articles.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return BlogTile(
+                              articles[index].urlToImage,
+                              articles[index].title,
+                              articles[index].description,
+                              articles[index].url);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              )),
     );
   }
 }
@@ -171,9 +160,12 @@ class CategoryTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryNews(
-          category: categoryname.toString().toLowerCase(),
-        )));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => CategoryNews(
+                      category: categoryname.toString().toLowerCase(),
+                    )));
       },
       child: Container(
         margin: EdgeInsets.only(right: 10),
@@ -188,7 +180,6 @@ class CategoryTile extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-
             Container(
               alignment: Alignment.center,
               width: 120,
@@ -207,31 +198,49 @@ class CategoryTile extends StatelessWidget {
 }
 
 class BlogTile extends StatelessWidget {
-  final String imageUrl, title, desc,url;
+  final String imageUrl, title, desc, url;
 
-  BlogTile(@required this.imageUrl, @required this.title, @required this.desc, @required this.url);
+  BlogTile(@required this.imageUrl, @required this.title, @required this.desc,
+      @required this.url);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return ArticleView(blogUrl: url,);
-        },));
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return ArticleView(
+              blogUrl: url,
+            );
+          },
+        ));
       },
       child: Container(
         margin: EdgeInsets.only(bottom: 20),
         child: Column(
-          children: [ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(imageUrl)),
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(imageUrl)),
             SizedBox(height: 8),
-            Text(title,style: TextStyle(fontWeight: FontWeight.w500,color: Colors.black87, fontSize: 17)),
-            SizedBox(height: 5,),
-            Text(desc,style: TextStyle(color: Colors.black54),
-            )],
+            Text(title,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black87,
+                    fontSize: 17)),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              desc,
+              style: TextStyle(color: Colors.black54),
+            )
+          ],
         ),
       ),
     );
   }
 }
+
+
+
